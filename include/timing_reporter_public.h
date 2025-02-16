a9670229d56c6687dac27914d31134c926ca4659
TimoGuy <timoguy99004@gmail.com>
2025-02-15 17:05:30 -0800

    Adds macro magic header file.

---
+++ include/timing_reporter_public.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Std libs required for these macros to work.
+#include <chrono>
+#include <iostream>
+
+
+#define TIMING_REPORT_START(name) auto timing_report_start_##name{ std::chrono::high_resolution_clock::now() }
+
+#define TIMING_REPORT_END_AND_PRINT(name, msg_before)                                        \
+do                                                                                           \
+{                                                                                            \
+    auto duration{ std::chrono::high_resolution_clock::now() - timing_report_start_##name }; \
+    std::cout << msg_before << duration << std::endl;                                        \
+} while (false)
