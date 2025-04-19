4152efd620aeec4959e3cffb5b8cddf658238dbf
TimoGuy <timoguy99004@gmail.com>
2025-04-18 23:22:47 -0700

    Disable timing reports.

--- include/timing_reporter_public.h
+++ include/timing_reporter_public.h
@@ -4,6 +4,10 @@
 #include <chrono>
 #include <iostream>
 
+#define TIMING_REPORT_ENABLE 0
+
+
+#if TIMING_REPORT_ENABLE
 
 #define TIMING_REPORT_START(name) auto timing_report_start_##name{ std::chrono::high_resolution_clock::now() }
 
@@ -13,3 +17,10 @@ do                                                                              
     auto duration{ std::chrono::high_resolution_clock::now() - timing_report_start_##name }; \
     std::cout << msg_before << duration << std::endl;                                        \
 } while (false)
+
+#else  // TIMING_REPORT_ENABLE
+
+#define TIMING_REPORT_START(name)
+#define TIMING_REPORT_END_AND_PRINT(name, msg_before)
+
+#endif  // TIMING_REPORT_ENABLE
